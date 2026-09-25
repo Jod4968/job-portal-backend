408db6d7f609e7d04ae53395a387529469c4d913
CrackedNuts54 <sagargusain96341@gmail.com>
2026-09-25 15:22:35 +0530

    add user entity

--- src/main/java/com/shelfnotes/jobportal/entity/User.java
+++ src/main/java/com/shelfnotes/jobportal/entity/User.java
@@ -1,4 +1,39 @@
 package com.shelfnotes.jobportal.entity;
 
+import com.shelfnotes.jobportal.enums.Role;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "Username cannot be empty")
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @NotBlank(message = "Name cannot be empty")
+    private String name;
+
+    @Email(message = "Must contain a valid email")
+    @NotBlank(message = "Email cannot be empty")
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Size(min = 8, message = "Password must contain at least 8 characters")
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
